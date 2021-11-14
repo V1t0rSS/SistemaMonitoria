85a2664e7d9566f9963ba0c3485f5e67a416b8fb
Renato <renato@hiparc.com>
2021-11-14 12:46:43 -0300

    inicio do ambiente administrativo

---
+++ admin/includes/scripts_footer.php
@@ -0,0 +1,7 @@
+<!-- bootstrap js -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<!-- chart js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.0/chart.min.js" integrity="sha512-GMGzUEevhWh8Tc/njS0bDpwgxdCJLQBWG3Z2Ct+JGOpVnEmjvNx6ts4v6A2XJf1HOrtOsfhv3hBKpK9kE5z8AQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- custom js -->
+<script src="./assets/js/index.js"></script>
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,42 @@
+<div class="l-navbar" id="nav-bar">
+    <nav class="nav">
+        <div>
+            <a href="#" class="nav_logo">
+                <i class='bx bxs-graduation nav_logo-icon'></i>
+                <span class="nav_logo-name">SistemaMonitoria</span>
+            </a>
+            <div class="nav_list">
+                <a href="#" class="nav_link active">
+                    <i class='bx bx-home nav_icon'></i>
+                    <span class="nav_name">Início</span>
+                </a>
+                <a href="#" class="nav_link">
+                    <i class='bx bx-message-square-detail nav_icon'></i>
+                    <span class="nav_name">Chamados 
+                        <span class="badge rounded-pill bg-danger">2</span>
+                    </span>
+                </a>
+                <a href="#" class="nav_link">
+                    <i class='bx bx-user nav_icon'></i>
+                    <span class="nav_name">Monitorias</span>
+                </a>
+                <a href="#" class="nav_link">
+                    <i class='bx bx-bookmark nav_icon'></i>
+                    <span class="nav_name">Notícias</span>
+                </a>
+                <a href="#" class="nav_link">
+                    <i class='bx bxs-user-account'></i>
+                    <span class="nav_name">Alunos</span>
+                </a>
+                <a href="#" class="nav_link">
+                    <i class='bx bxs-user-account'></i>
+                    <span class="nav_name">Monitores</span>
+                </a>
+                <a href="#" class="nav_link text-danger">
+                    <i class='bx bx-log-out'></i>
+                    <span class="nav_name">Sair</span>
+                </a>
+            </div>
+        </div>
+    </nav>
+</div>
