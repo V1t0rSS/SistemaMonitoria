749d50f5b1f458c21decdf99282be341646af215
Rodrigo Raider <80008624+rraidero@users.noreply.github.com>
2021-12-12 16:13:41 -0300

    Create controleUsuarios.js

---
+++ web/assets/js/controleUsuarios.js
@@ -0,0 +1,49 @@
+controleUsuario = new ControleUsuarios();
+
+function ControleUsuarios() {
+
+    this.login = function (event) {
+        event.preventDefault();//previne que o browser faça a submissao, premitindo que seja feita com javascript.
+        var formElement = document.getElementById("login_aluno");
+        //https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+        //var tarefaForm = new FormData(formElement);
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function () {
+			alert("teste");
+            if (xmlhttp.readyState === xmlhttp.DONE) {
+                if (xmlhttp.status === 200)
+                {   //controleTarefa.get();
+                    alert("Usuario logado com sucesso");
+                    //addLogout();
+                    //document.getElementById("login_modal").style = 'none';
+                } else {
+                    alert("Usuario ou senha incorretos");
+                }
+            }
+        };
+        //alert("test2e");
+        xmlhttp.open("POST", "/api/alunos.php/login");
+        //xmlhttp.send(tarefaForm);
+    };
+    
+    
+     this.logout = function (event) {
+        event.preventDefault();//previne que o browser faça a submissao, premitindo que seja feita com javascript.
+       var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function () {
+            if (xmlhttp.readyState === xmlhttp.DONE) {
+                if (xmlhttp.status === 200)
+                {
+                      carregaListaTarefas([]);
+                    alert("Deslogado com sucesso");
+                    addLogin();
+                } else {
+                    alert("Nao foi possivel deslogar");
+                }
+            }
+        };
+        xmlhttp.open("POST", "/api/Alunos.php/logout");
+        xmlhttp.send();
+    };
+   
+}
