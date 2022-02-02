56a3e84db90a410a518a7d5f9778a18bc5508d20
Renato <renato@hiparc.com>
2022-02-02 02:28:52 -0300

    acertando merge

--- web/aluno/includes/header.php
+++ web/aluno/includes/header.php
@@ -8,11 +8,7 @@
         <a id="logo" href="index.php">MonitoriasOnline</a>
     </span>
 
-<<<<<<< HEAD
     <a  href="<?= $basepath ?>/web/aluno/includes/sair.php">Logout</a>
-=======
-    <a  href="../includes/sair.php">Logout</a>
->>>>>>> feature/adminLogin
     <a  href="<?= $basepath ?>/web/aluno/monitorias">Monitorias</a>
     <a  href="<?= $basepath ?>/web/aluno/dashboard">Eventos</a>
 </nav>
